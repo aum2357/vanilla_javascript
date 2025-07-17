ae2d21809a5ad80db90f0f167b7f14aecd289224
aum2357 <ahambrahmasmi2357@gmail.com>
2025-07-17 08:34:04 +0530

    add comments in optional chaining

--- optional_chaining.js
+++ optional_chaining.js
@@ -1,3 +1,13 @@
+/*
+The optional chaining ?. , ?.() , ?.[] is a safe way to access  object properties,
+even if they don’t exist.
+if you access them conventionally you'll get an error
+ */
+let user = {}; // a user without "address" property
+
+console.log(user?.address?.street); // undefined
+console.log(user.address.street); // Error!
+
 let userAdmin = {
     admin() {
         console.log("I am admin");
@@ -8,6 +18,7 @@ let userGuest = {};
 
 userAdmin.admin?.(); // I am admin
 
+userGuest.admin?.(); // nothing happens (no such method) otherwise an error
 
 let key = "firstName";
 
@@ -17,4 +28,5 @@ let user1 = {
 
 let user2 = null;
 
-console.log( user1?.[key] ); // John
+console.log(user1?.[key] ); // John
+console.log(user2?.[key] ); // undefined ,otherwise error
