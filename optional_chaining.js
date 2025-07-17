adf164022b8c42221c86ed8eac062395177e7cac
aum2357 <ahambrahmasmi2357@gmail.com>
2025-07-17 08:15:40 +0530

    optional chaining

--- optional_chaining.js
+++ optional_chaining.js
@@ -1,14 +1,3 @@
-// ?.    -> with properties of objects
-// ?.()  -> with methods
-// ?.[]  -> with keys
-// The optional chaining ?. is a safe way to access object properties,methods,keys
-// even if property,method ,key doesn’t exist.
-
-// in such cases if you access the keys,properties or methods as usual you'll get error
-// but by using this you'll get undefined that's okay.
-let user = {};
-console.log(user?.address?.street);// undefined otherwise error
-
 let userAdmin = {
     admin() {
         console.log("I am admin");
@@ -19,7 +8,6 @@ let userGuest = {};
 
 userAdmin.admin?.(); // I am admin
 
-userGuest.admin?.(); // nothing happens (no such method)
 
 let key = "firstName";
 
@@ -30,5 +18,3 @@ let user1 = {
 let user2 = null;
 
 console.log( user1?.[key] ); // John
-console.log( user2?.[key] ); // undefined
-// console.log(user2.[key]); // error
